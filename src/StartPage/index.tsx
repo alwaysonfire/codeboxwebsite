16086d4df52044b3c06617b92549a1ef0fe1e4d3
euginetan <alwaysonfire2@gmail.com>
2022-11-10 17:28:50 +0800

    Commit for new build

--- src/StartPage/index.tsx
+++ src/StartPage/index.tsx
@@ -2,16 +2,26 @@ import { Button, Box, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { minHeight } from '@mui/system';
 import Background from './startPage.png';
+import MobileBackGround from '../background/codeBoxMobile.jpg';
 import logo from './codeBoxLogo.png';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
 import useMatchMedia from 'use-match-media-hook';
 
-const queries = ['(max-width: 400px)', '(min-width: 800px)'];
+const queries = ['(max-width: 400px)', '(min-width: 460px)'];
 
 const Base = styled('div')({
   color: 'white',
   backgroundImage: `url(${Background})`,
+  minHeight: '100vh',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+});
+
+const Mobile = styled('div')({
+  color: 'white',
+  backgroundImage: `url(${MobileBackGround})`,
   minHeight: '100vh',
   backgroundRepeat: 'no-repeat',
   backgroundPosition: 'center',
@@ -53,64 +63,141 @@ const StartPage = () => {
 
   return (
     <motion.div variants={containerVariants} initial="hidden" animate="visible">
-      <Base>
+      {desktop ? (
         <>
-          {' '}
-          <Box
-            component={motion.div}
-            sx={{ minHeight: '100vh' }}
-            justifyContent={'center'}
-            alignItems={'center'}
-            display={'flex'}
-            variants={childVariants}
-            position="relative"
-            bottom="100px"
-          >
-            <Box textAlign={'center'}>
-              <Box
-                sx={{
-                  height: { xs: 150, md: 177, lg: 200 },
-                  width: { xs: 260, md: 290, lg: 330 },
-                  maxHeight: { xs: 150, md: 177, lg: 300 },
-                  maxWidth: { xs: 260, md: 290, lg: 330 },
-                }}
-                component={'img'}
-                src={logo}
-                alt="codebox logo"
-              />
+          <Base>
+            {' '}
+            <Box
+              component={motion.div}
+              sx={{ minHeight: '100vh' }}
+              justifyContent={'center'}
+              alignItems={'center'}
+              display={'flex'}
+              variants={childVariants}
+              position="relative"
+              bottom="100px"
+            >
+              <Box textAlign={'center'}>
+                <Box
+                  sx={{
+                    height: { xs: 150, md: 177, lg: 200 },
+                    width: { xs: 260, md: 290, lg: 330 },
+                    maxHeight: { xs: 150, md: 177, lg: 300 },
+                    maxWidth: { xs: 260, md: 290, lg: 330 },
+                  }}
+                  component={'img'}
+                  src={logo}
+                  alt="codebox logo"
+                />
 
-              <Typography
-                sx={{ position: 'relative', bottom: '33px' }}
-                color={'white'}
-                fontFamily={'Poppins'}
-              >
-                Getting dev sh*t done!
-              </Typography>
-              <Box sx={{ mt: 1 }}>
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    navigate('/landing');
-                  }}
-                  component={motion.div}
-                  sx={{ backgroundColor: '#df00cd', borderRadius: 5, px: 4 }}
-                  whileHover={{
-                    scale: 1.2,
-                    textShadow: '0px 0px 8px rgb(255,255,255)',
-                    boxShadow: '0px 0px 8px rgb(255,255,255)',
-                    transition: {
-                      repeat: Infinity,
-                      duration: 0.8,
-                    },
-                  }}
+                <Typography
+                  sx={{ position: 'relative', bottom: '33px' }}
+                  color={'white'}
+                  fontFamily={'Poppins'}
                 >
-                  Explore
-                </Button>
+                  Getting dev sh*t done!
+                </Typography>
+                <Box sx={{ mt: 1 }}>
+                  <Button
+                    variant="contained"
+                    onClick={() => {
+                      navigate('/landing');
+                    }}
+                    component={motion.div}
+                    sx={{
+                      borderRadius: 5,
+                      px: 4,
+                    }}
+                    animate={{
+                      backgroundColor: '#df00cd',
+                    }}
+                    whileHover={{
+                      scale: 1.2,
+                      textShadow: '0px 0px 8px rgb(255,255,255)',
+                      boxShadow: '0px 0px 8px rgb(255,255,255)',
+                      transition: {
+                        repeat: Infinity,
+                        duration: 0.8,
+                      },
+                    }}
+                  >
+                    Explore
+                  </Button>
+                </Box>
               </Box>
             </Box>
-          </Box>
+          </Base>
         </>
-      </Base>
+      ) : (
+        <>
+          {' '}
+          <Mobile>
+            {' '}
+            <Box
+              component={motion.div}
+              sx={{ minHeight: '100vh' }}
+              justifyContent={'center'}
+              alignItems={'center'}
+              display={'flex'}
+              variants={childVariants}
+              position="relative"
+            >
+              <Box textAlign={'center'}>
+                <Box
+                  sx={{
+                    height: { xs: 145, md: 177, lg: 200 },
+                    width: { xs: 250, md: 290, lg: 330 },
+                    maxHeight: { xs: 145, md: 177, lg: 300 },
+                    maxWidth: { xs: 250, md: 290, lg: 330 },
+                  }}
+                  component={'img'}
+                  src={logo}
+                  alt="codebox logo"
+                />
+
+                <Typography
+                  sx={{
+                    position: 'relative',
+                    bottom: '33px',
+                    fontSize: '0.8rem',
+                  }}
+                  color={'white'}
+                  fontFamily={'Poppins'}
+                >
+                  Getting dev sh*t done!
+                </Typography>
+                <Box sx={{ mt: 1 }}>
+                  <Button
+                    variant="contained"
+                    onClick={() => {
+                      navigate('/landing');
+                    }}
+                    component={motion.div}
+                    sx={{
+                      borderRadius: 5,
+                      px: 3,
+                    }}
+                    animate={{
+                      backgroundColor: '#df00cd',
+                    }}
+                    whileHover={{
+                      scale: 1.2,
+                      textShadow: '0px 0px 8px rgb(255,255,255)',
+                      boxShadow: '0px 0px 8px rgb(255,255,255)',
+                      transition: {
+                        repeat: Infinity,
+                        duration: 0.8,
+                      },
+                    }}
+                  >
+                    Explore
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
+          </Mobile>
+        </>
+      )}
     </motion.div>
   );
 };
